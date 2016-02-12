f626681c193facfcc2c903a92509786c0f0a7936
Stanislaw Antol <antolstan@gmail.com>
2016-02-12 03:14:55 -0500

    Updated configuration to not be in debug mode.

--- server/config/settings.py
+++ server/config/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
+#DEBUG = True
+ALLOWED_HOSTS = ['*'] # Change this for better security?
 
 # Application definition
 
